71dcbc280ce4093a28672358b865a5f4be37c33b
Patrick Rohr <patrickrohr92@gmail.com>
2018-04-12 00:36:53 -0400

    Optimizes const_map. Still need to find a way to fail compilation.

--- ConstexprMap/map.cpp
+++ ConstexprMap/map.cpp
@@ -17,32 +17,24 @@ struct map_entry
  
     // Member Functions
     constexpr map_entry(const K& _key, const V& _val) :
-        key{_key},
-        val{_val}
-    {}
+        key(_key),
+        val(_val)
+    {
+    }
 
     const key_type key;
     const value_type val;
 };
 
-
-using size_type = unsigned;
-template<typename K, typename V, size_type index>
+template<typename K, typename V, unsigned index>
 struct const_map
-{
-    // using value_type = std::pair<K, V>;
-    
-    // static constexpr size_type size = sizeof...(Args);
-
-    // static constexpr std::array<value_type, N> data;
-    // static constexpr value_type data[size] = { Args... };
-    
+{    
     using value_type = map_entry<K, V>;
 
     template<class ...Ts>
     constexpr const_map(value_type _data, Ts... params) :
-        data{_data.key, _data.val},
-        tail{params...}
+        data(_data.key, _data.val),
+        tail(params...)
     {
     }
 
@@ -51,8 +43,8 @@ struct const_map
         return data.key == key ? data.val : tail.find(key);
     }
 
-    value_type data;
-    const_map<K, V, index - 1> tail;
+    const value_type data;
+    const const_map<K, V, index - 1> tail;
 };
 
 template<typename K, typename V>
@@ -60,24 +52,24 @@ struct const_map<K, V, 1>
 {
     constexpr const_map(map_entry<K, V> _data) :
         data{_data}
-    {}
-
-    void compile_time_error()
     {
-        // do nothing
+    }
+    
+    constexpr V find(const K& key) const noexcept
+    {
+        return data.key == key ? data.val : 0; // throw 0; // Somehow this is not working with clang...TODO: fail
     }
 
-    constexpr V find(const K& key) const
-    {
-        return data.key == key ? data.val : throw std::out_of_range("key not found");
-    }
-
-    map_entry<K, V> data;
+    const map_entry<K, V> data;
 };
 
 
 int main()
 {
-    const_map<int, int, 2> map{ map_entry<int, int>({1, 2}), map_entry<int, int>({2, 3})};
-    std::cout << map.find(1) << std::endl;
+    map_entry<int, int>(1, 2);
+
+
+    constexpr const_map<int, int, 1> map(map_entry<int, int>(1, 2));
+    std::integral_constant<int, map.find(2)> c;
+    std::integral_constant<int, map.find(3)> c2;
 }
